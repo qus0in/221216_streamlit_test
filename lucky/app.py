4c778535f62b80e9b90f5865a1d3d5c8707219e4
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-13 12:43:53 +0900

    4

--- lucky/app.py
+++ lucky/app.py
@@ -15,7 +15,9 @@ columns = st.columns(4) # 화면을 열로 나누어서 배치
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
 for idx, col in enumerate(columns):
-    col.text_input(f"조 추첨 대상 {idx+1}", key=idx)
+    # col.text_input(f"조 추첨 대상 {idx+1}", key=idx)
+    for idx2 in range(4):
+        col.text_input(f"조 추첨 대상 {idx+1 + idx2 * 4}", key=idx)
 # 13명이 소속될 조 이름을 넣을 위치
 
 # <추첨 버튼>
