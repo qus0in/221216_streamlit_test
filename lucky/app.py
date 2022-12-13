a35bdd8271ede6bde1822930a8fd3da58d8c3f2f
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-13 14:41:52 +0900

    1232123

--- lucky/app.py
+++ lucky/app.py
@@ -49,36 +49,37 @@ for idx, col in enumerate(columns2): # 열의 위치
             key=f"g{idx+1 + idx2 * 4}" # n -> g => 겹치지 말라고
         ) # 4번 호출됨
 
-# 13명이 소속될 조 이름을 넣을 위치
-# st.write(st.session_state)
-# np.random.choice -> 추출해서 이름들, 목록
-# 1. st.session_state - n, g가 섞여있음
-ss = pd.Series(st.session_state) # 딕셔너리 -> 시리즈
-# st.write(ss)
-# ss2 = ss[ss != ""]
-ss2 = ss[ss.ne("")]
-# st.write(ss2)
-
-# str: string 관련된 메소드를 사용할 수 있게 함
-n_idx = ss2.index.str.contains('n')
-n_data = ss2[n_idx]
-# st.write(n_data)
-
-g_idx = ss2.index.str.contains('g')
-g_data = ss2[g_idx]
-# st.write(g_data)
-
-# n_data를 섞어줄 것임 (비복원으로)
-n_rd = np.random.choice(n_data, len(n_data), replace=False)
-# st.write(n_rd)
-g_rd = np.random.choice(g_data, len(g_data), replace=False)
-# st.write(g_rd)
-
-# 2. df 형태로 정리
-df = pd.DataFrame({
-    "추첨 대상자 이름": n_rd,
-    "조 이름": g_rd,
-})
-st.write(df)
 # <추첨 버튼>
-# 13개의 짝을 지어서 표시해줄 그래픽
+if st.button('추첨 시작!'):
+    # 13명이 소속될 조 이름을 넣을 위치
+    # st.write(st.session_state)
+    # np.random.choice -> 추출해서 이름들, 목록
+    # 1. st.session_state - n, g가 섞여있음
+    ss = pd.Series(st.session_state) # 딕셔너리 -> 시리즈
+    # st.write(ss)
+    # ss2 = ss[ss != ""]
+    ss2 = ss[ss.ne("")]
+    # st.write(ss2)
+
+    # str: string 관련된 메소드를 사용할 수 있게 함
+    n_idx = ss2.index.str.contains('n')
+    n_data = ss2[n_idx]
+    # st.write(n_data)
+
+    g_idx = ss2.index.str.contains('g')
+    g_data = ss2[g_idx]
+    # st.write(g_data)
+
+    # n_data를 섞어줄 것임 (비복원으로)
+    n_rd = np.random.choice(n_data, len(n_data), replace=False)
+    # st.write(n_rd)
+    g_rd = np.random.choice(g_data, len(g_data), replace=False)
+    # st.write(g_rd)
+
+    # 2. df 형태로 정리
+    # 13개의 짝을 지어서 표시해줄 그래픽
+    df = pd.DataFrame({
+        "추첨 대상자 이름": n_rd,
+        "조 이름": g_rd,
+    })
+    st.write(df)
