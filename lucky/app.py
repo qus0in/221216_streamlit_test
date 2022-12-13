07ef999eaaf41689e21a4e64e405df210756b562
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-13 14:15:55 +0900

    1

--- lucky/app.py
+++ lucky/app.py
@@ -1,25 +1,32 @@
+# https://docs.streamlit.io/library/get-started/main-concepts
+# https://docs.streamlit.io/library/cheatsheet
 # streamlit 라이브러리 호출
-import streamlit as st
+import streamlit as st # st라는 이름으로 사용
 import numpy as np
 
-# https://docs.streamlit.io/library/get-started/main-concepts
 # st.write() 마크다운
 st.title("조추첨 페이지")
 st.header("여러분의 참여를 환영합니다!")
-# https://docs.streamlit.io/library/cheatsheet
 
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
-# 3 x 4 (row, col)
+# 4 x 4 (row, col)
 # 열을 배치하는 메소드
+# x = st.columns(n) : n만큼의 컬럼 리스트를 생성
 columns = st.columns(4) # 화면을 열로 나누어서 배치
 # 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
-for idx, col in enumerate(columns):
+# enumerate : index, value 묶음
+for idx, col in enumerate(columns): # 열의 위치
+    # 이중 for문 (for문 안에 for문)
     # col.text_input(f"조 추첨 대상 {idx+1}", key=idx)
     for idx2 in range(4):
         # key가 겹치면 안 됨
-        col.text_input(f"조 추첨 대상 {idx+1 + idx2 * 4}", key=f"{idx+1 + idx2 * 4}")
+        # col 안에 메소드를 통해서 요소들을 생성해주겠다
+        col.text_input(
+            f"조 추첨 대상 {idx+1 + idx2 * 4}",
+            key=f"{idx+1 + idx2 * 4}"
+        ) # 4번 호출됨
 # 13명이 소속될 조 이름을 넣을 위치
-
+st.write(st.session_state)
 # <추첨 버튼>
 # 13개의 짝을 지어서 표시해줄 그래픽
