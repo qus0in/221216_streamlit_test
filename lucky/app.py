44284f5ecbd152fc3d5c795eb157d943c6d94f1c
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-13 12:41:52 +0900

    3

--- lucky/app.py
+++ lucky/app.py
@@ -11,10 +11,11 @@ st.header("여러분의 참여를 환영합니다!")
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
 # 3 x 4 (row, col)
 # 열을 배치하는 메소드
-columns = st.columns(4)
+columns = st.columns(4) # 화면을 열로 나누어서 배치
+# 가로 4개의 열 -> columns = [col1, col2, col3, col4]
 # col1, col2, col3, col4
-for idx, c in enumerate(columns):
-    st.text_input("조 추첨 대상", key=idx)
+for idx, col in enumerate(columns):
+    col.text_input("조 추첨 대상", key=idx)
 # 13명이 소속될 조 이름을 넣을 위치
 
 # <추첨 버튼>
