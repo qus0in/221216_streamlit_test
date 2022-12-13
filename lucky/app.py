72627855acac72a8112b05551cef50b281bb3f7e
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-13 12:33:50 +0900

    st.header

--- lucky/app.py
+++ lucky/app.py
@@ -5,6 +5,7 @@ import numpy as np
 # https://docs.streamlit.io/library/get-started/main-concepts
 # st.write() 마크다운
 st.title("조추첨 페이지")
+st.header("여러분의 참여를 환영합니다!")
 
 # 추첨 대상인 13명의 이름을 넣을 수 있는 text_input
 # 13명이 소속될 조 이름을 넣을 위치
