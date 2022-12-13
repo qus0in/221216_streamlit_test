f66c7d6d96294bdffa3f31e3be2edf1aeecd7bf8
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-13 15:26:48 +0900

    12313

--- graph/app.py
+++ graph/app.py
@@ -36,3 +36,10 @@ st.pyplot(fig)
 fig = plt.figure(figsize=(8, 4))
 sns.histplot(data=titanic, x='age', hue='alive', multiple='stack')
 st.pyplot(fig)
+
+# pie
+x = [10, 60, 30] # 범주형 데이터별 파이 그래프의 비율
+labels = ['A', 'B', 'C']
+fig = plt.figure(figsize=(8, 4))
+plt.pie(x=x, labels=labels, autopct='%.1f%%')
+st.pyplot(fig)
