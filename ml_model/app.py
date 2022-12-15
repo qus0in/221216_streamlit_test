feb41d70dc28cd688fb7a268b27353ba2aeefdb9
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-15 17:35:40 +0900

    1234

--- ml_model/app.py
+++ ml_model/app.py
@@ -65,7 +65,20 @@ st.selectbox(
     index=2,
     key='region'
 )
-st.write(st.session_state['region'])
+# st.write(st.session_state['region'])
 
 if st.button('예측'):
     st.balloons()
+    # 예측
+    # model.predict(X_test) -> 전처리한 데이터 형태로 들어간 행렬, df.
+    # df X -> 이중 리스트 ([])
+    # [ [age,bmi,children,smoker,sex_male,
+    #    region_northwest,region_northeast,region_southwest] ]
+    state = st.session_state
+    input_values = [[
+        state['age'], state['bmi'], state['children'], state['smoker'],
+        state['sex'] == '남성', state['region'] == '북서',
+        state['region'] == '북동', state['region'] == '남서'
+    ]]
+    pred = model.predict(input_values)
+    st.write(pred[0])
