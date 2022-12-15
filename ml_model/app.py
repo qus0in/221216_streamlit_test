002b999e442a8083ad7565afb7da26bb82eda511
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-15 17:18:23 +0900

    age

--- ml_model/app.py
+++ ml_model/app.py
@@ -7,8 +7,18 @@ st.write(df)
 
 import joblib
 import os
-
+# os.path... 파이썬 경로문제 해결
 model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
 model = joblib.load(model_path)
 st.write("## 선형 회귀 모델")
 st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
+
+# age : 나이
+st.number_input(
+    label="나이",
+    step=1, 
+    value=30,
+    key='age'
+)
+# st.session_state['age']
+st.write(st.session_state['age'])
