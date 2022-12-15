5e8300db1fd06d97414ffe9c84583aa22ea87bda
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-15 17:03:07 +0900

    ml model

---
+++ ml_model/app.py
@@ -0,0 +1,6 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
+st.write(df)
