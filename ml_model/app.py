3e70ba478b58b494953bad3e425ed01cc177f047
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-15 17:11:11 +0900

    123123

--- ml_model/app.py
+++ ml_model/app.py
@@ -6,5 +6,8 @@ df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/d
 st.write(df)
 
 import joblib
-model = joblib.load('./ml_model/app.py')
-st.write(model.coef_)
+
+model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
+model = joblib.load(model_path)
+st.write("## 선형 회귀 모델")
+st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
