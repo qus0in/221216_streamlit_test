60b0abe2a71827afe77e5659da00c7966c5bca30
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-15 17:07:58 +0900

    12313

--- ml_model/app.py
+++ ml_model/app.py
@@ -4,3 +4,8 @@ import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
 st.write(df)
+
+import joblib
+model = joblib.load('./ml_model/app.py')
+model_info = pd.Series(model.coef_, index = df.drop['expenses'].columns)
+st.write(model_info)
