a348017f01fc942948f6feca6e64c38e2df885e2
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2022-12-16 11:10:25 +0900

    12123

--- ml_model/app.py
+++ ml_model/app.py
@@ -12,13 +12,14 @@ with st.echo():
     df = pd.read_csv('https://raw.githubusercontent.com/bigdata-young/ai_26th/main/data/insurance.csv')
     st.write(df)
 
-import joblib # 사이킷런 import 안해도 model 객체 자체를 pkl로 불러옴
-import os
-# os.path... 파이썬 경로문제 해결
-model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
-model = joblib.load(model_path)
-st.write("## 선형 회귀 모델")
-st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
+with st.echo():
+    import joblib # 사이킷런 import 안해도 model 객체 자체를 pkl로 불러옴
+    import os
+    # os.path... 파이썬 경로문제 해결
+    model_path = f"{os.path.dirname(os.path.abspath(__file__))}/model.pkl"
+    model = joblib.load(model_path)
+    st.write("## 선형 회귀 모델")
+    st.write(pd.Series(model.coef_, index=["age", "bmi", "children", "smoker", "sex_male", "region_northwest", "region_northeast", "region_southwest"]))
 
 # age : 나이
 st.number_input(
